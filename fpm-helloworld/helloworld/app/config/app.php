3f7b08667a0d96683fd9f6679f1a9cdbf13f1b79
Andrey Stolbovsky <andrey.stolbovsky@gmail.com>
2014-11-27 10:40:54 +0300

    Add SampleService package

--- fpm-helloworld/helloworld/app/config/app.php
+++ fpm-helloworld/helloworld/app/config/app.php
@@ -121,6 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Krz\SampleService\SampleServiceServiceProvider'
 
 	),
 
